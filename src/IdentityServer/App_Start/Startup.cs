14483352d38e2274aaee17acd10946e0cc1294d7
zchhaenngg <1076968967@qq.com>
2017-06-23 23:32:29 +0800

    init

---
+++ src/IdentityServer/App_Start/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using IdentityServer.IdentityServer;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Models;
+using Microsoft.Owin;
+using Owin;
+using static IdentityServer3.Core.Constants;
+
+[assembly: OwinStartup(typeof(IdentityServer.App_Start.Startup))]
+
+namespace IdentityServer.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.Map("/identity", idsrvApp =>
+            {
+                idsrvApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Embedded IdentityServer",
+                    //SigningCertificate = LoadCertificate(),
+
+                    Factory = new IdentityServerServiceFactory()
+                                .UseInMemoryUsers(Users.Get())
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(IdentityServer3.Core.Models.StandardScopes.All)
+                });
+            });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\IdentityServer\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+    }
+}
---
+++ src/IdentityServer/IdentityServer/Clients.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IdentityServer3.Core.Models;
+
+namespace IdentityServer.IdentityServer
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+           {
+                new Client
+                {
+                    ClientName = "MVC Client",
+                    ClientId = "mvc",
+                    Flow = Flows.Implicit,
+
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:44329/"
+                    },
+                    AllowAccessToAllScopes = true
+                }
+            };
+        }
+    }
+}
